fe9becfa9855a709ca2f60535f48587ac890fc7a
nain <onyu1221@gmail.com>
2024-05-13 11:23:34 +0900

    [Feat] Nain 뷰 제작

---
+++ GitHub Practice 2024/GitHub Practice 2024/View/LetterViews/NainView.swift
@@ -0,0 +1,18 @@
+//
+//  NainView.swift
+//  GitHub Practice 2024
+//
+//  Created by jeongonyu on 5/13/24.
+//
+
+import SwiftUI
+
+struct NainView: View {
+    var body: some View {
+        Text("오예")
+    }
+}
+
+#Preview {
+    NainView()
+}
